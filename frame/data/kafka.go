0a7ce784d46d2d7640dfc7c9ae3822d17899bd12
lvfei <lvfei@xbonline.com>
2020-05-18 20:50:40 +0800

    增加新版本kafka消费方法

--- frame/data/kafka.go
+++ frame/data/kafka.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
 	"sync"
 
@@ -153,6 +154,48 @@ func KafkaConsume(signals chan os.Signal, topic, groupID string, errCallback fun
 			kafkaConsumer.MarkOffset(msg, "") // mark message as processed
 			callback(msg.Value)
 		case <-signals:
+			return
+		}
+	}
+}
+
+// KafkaConsumeV1 带处理停止信号的 kafka 消费
+func KafkaConsumeV1(signals chan os.Signal, topics []string, groupID string, errCallback func(err error),
+	callback func(msgBytes []byte), finishCallback func()) {
+	kafkaConsumer, err := NewConsumerWithNewestOffset(topics, groupID)
+	if err != nil {
+		panic(err)
+	}
+
+	defer kafkaConsumer.Close()
+
+	// consume errors
+	go func() {
+		for err := range kafkaConsumer.Errors() {
+			errCallback(err)
+		}
+	}()
+
+	// consume notifications
+	go func() {
+		for range kafkaConsumer.Notifications() {
+		}
+	}()
+
+	// consume messages, watch signals
+	for {
+		select {
+		case msg, ok := <-kafkaConsumer.Messages():
+			if !ok {
+				break
+			}
+
+			kafkaConsumer.MarkOffset(msg, "") // mark message as processed
+			callback(msg.Value)
+		case s := <-signals:
+			signals <- s
+			fmt.Println("stop starting....", len(signals))
+			finishCallback()
 			return
 		}
 	}
