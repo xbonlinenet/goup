3b807851a1ffc5d5755186be61bee0e1f1492bb1
leo <bluexiangzhou@gmail.com>
2022-04-14 11:30:16 +0800

    fix: KafkaConsumeV3 需要使用 NewConsumerWithNewestOffset，赋值CommitInterval

--- frame/data/kafka.go
+++ frame/data/kafka.go
@@ -248,7 +248,7 @@ func KafkaConsumeV2(signals chan os.Signal, topics []string, groupID string, err
 // KafkaConsumeV3 带处理context停止信号的 kafka 消费
 func KafkaConsumeV3(ctx context.Context, topics []string, groupID string, errCallback func(err error),
 	callback func(msgBytes []byte), finishCallback func()) {
-	kafkaConsumer, err := NewConsumer(topics, groupID)
+	kafkaConsumer, err := NewConsumerWithNewestOffset(topics, groupID)
 	if err != nil {
 		panic(err)
 	}
